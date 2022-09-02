239402ac458a2cd4602b52d5094ae6b2c6036581
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-09-02 18:21:40 +0300

    CSS Update

--- src/shop_layout.js
+++ src/shop_layout.js
@@ -15,21 +15,27 @@ class Shop {
         this.container.className = "PrettyCards_ShopContainer";
 
         this.left = document.createElement("DIV");
+        this.left.className = "PrettyCards_ShopLeft";
         this.container.appendChild(this.left);
 
         this.buyContainer = document.createElement("DIV");
+        this.buyContainer.className = "PrettyCards_ShopBuyContainer";
         this.left.appendChild(this.buyContainer);
 
         this.dialogueContainer = document.createElement("DIV");
+        this.dialogueContainer.className = "PrettyCards_ShopDialogueContainer";
         this.left.appendChild(this.dialogueContainer);
 
         this.right = document.createElement("DIV");
+        this.right.className = "PrettyCards_ShopRight";
         this.container.appendChild(this.right);
 
         this.shopkeeperContainer = document.createElement("DIV");
+        this.shopkeeperContainer.className = "PrettyCards_ShopkeeperContainer";
         this.right.appendChild(this.shopkeeperContainer);
 
         this.menuContainer = document.createElement("DIV");
+        this.menuContainer.className = "PrettyCards_ShopMenuContainer";
         this.right.appendChild(this.menuContainer);
         return this.container;
     }
