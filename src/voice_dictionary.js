d63588c102280b38fb015952e0dddc7069d58416
CMD-God <48065803+CMD-God@users.noreply.github.com>
2022-09-05 15:47:13 +0300

    Voice Testing Begins

---
+++ src/voice_dictionary.js
@@ -0,0 +1,24 @@
+
+class Voice {
+
+    constructor() {
+        this.sources = [];
+    }
+
+    AddSourceFast(name) {
+        this.AddSource(``)
+    }
+
+    AddSource(src) {
+        this.sources.push(src);
+    }
+
+    GetRandomSource() {
+
+    }
+
+}
+
+class VoiceDictionary {
+
+}
